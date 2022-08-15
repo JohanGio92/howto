7e67829bd72426fea9db08c1e4a6d301905626b9
Johan Revilla <johan_revilla_92@hotmail.com>
2022-08-14 23:52:00 -0300

    finish carrousel

---
+++ js/app.js
@@ -0,0 +1,48 @@
+
+class Carrousel {
+    static #MAX_CELL = 15;
+    static #REVOLUTION = 360;
+    static #RADIUS = 70.75;
+    static #selected = 0;
+
+    constructor() {
+        this.carrousel = this.querySelector(".carrousel");
+        this.previousButton = this.querySelector(".previous__button");
+        this.nextButton = this.querySelector(".next__button");
+        this.direction = {"left": -1, "right": 1};
+        this.rotateHandler = this.rotateHandler.bind(this);
+    }
+
+    querySelector(element){
+        return document.querySelector(element);
+    }
+
+    getTranslateZ(){
+        return `translateZ(${-Carrousel.#RADIUS}rem)`
+    }
+
+    getRotation(){
+        return `rotateY(${this.getAngle()}deg)`
+    }
+
+    getAngle(){
+        return Carrousel.#selected * (Carrousel.#REVOLUTION/Carrousel.#MAX_CELL);
+    }
+
+    turn(rotation){
+        Carrousel.#selected = Carrousel.#selected + this.direction[rotation];
+    }
+
+    rotateHandler(rotation){
+        this.turn(rotation);
+        console.log(this.getTranslateZ() + " " +this.getRotation());
+        this.carrousel.style.transform = this.getTranslateZ() + " " + this.getRotation();
+    }
+
+    execute(){
+        this.previousButton.addEventListener("click", () => this.rotateHandler("left"));
+        this.nextButton.addEventListener("click", () => this.rotateHandler("right"));
+    }
+}
+
+new Carrousel().execute();
